6c68451bc46fd5a62e0bf1df26899dca4439c3dd
nltimv <git@nltimv.com>
2023-11-28 15:44:09 +0100

    Various validations and improvements

--- karmabot.py
+++ karmabot.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import psycopg2
@@ -14,32 +15,46 @@ db_params = {
     "port": os.environ.get("DB_PORT"),
 }
 
-def update_karma(user_id, increment):
+def is_valid_user(user_id, token):
+    api_url = f"https://slack.com/api/users.info?user={user_id}"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.get(api_url, headers=headers)
+    user_info = response.json()
+
+    return user_info.get("ok", False)
+
+def update_karma(user_id, team_id, increment):
+    if not is_valid_user(user_id, os.environ.get("SLACK_BOT_TOKEN")):
+        return 0
+
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT karma FROM users WHERE user_id = %s", (user_id,))
+    cursor.execute("SELECT karma FROM users WHERE user_id = %s AND team_id = %s", (user_id, team_id))
     row = cursor.fetchone()
 
     if row is None:
-        cursor.execute("INSERT INTO users (user_id, karma) VALUES (%s, %s)", (user_id, increment))
-        conn.commit()
-        karma = increment
+        cursor.execute("INSERT INTO users (user_id, team_id, karma) VALUES (%s, %s, %s)", (user_id, team_id, increment))
     else:
         karma = row[0] + increment
-        cursor.execute("UPDATE users SET karma = %s WHERE user_id = %s", (karma, user_id))
-        conn.commit()
+        cursor.execute("UPDATE users SET karma = %s WHERE user_id = %s AND team_id = %s", (karma, user_id, team_id))
 
+    conn.commit()
     cursor.close()
     conn.close()
 
     return karma
 
-def get_karma(user_id):
+def get_karma(user_id, team_id):
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT karma FROM users WHERE user_id = %s", (user_id,))
+    cursor.execute("SELECT karma FROM users WHERE user_id = %s AND team_id = %s", (user_id, team_id))
     row = cursor.fetchone()
 
     cursor.close()
@@ -47,57 +62,61 @@ def get_karma(user_id):
 
     return row[0] if row else 0
 
-@app.message(re.compile(".*<@([a-zA-Z0-9_]+)>\\s?(\\+\\+\\+|\\-\\-\\-).*"))
-def process_double_karma_message(say, context):
-    user_id = context.matches[0]
-    action = context.matches[1]
-    current_user = context.user_id
-
-    if user_id == current_user and action == "+++":
-        say("Nice try! You can't boost your own ego. 😜")
-        return
-
-    increment = 2 if action == "+++" else -2
-    karma = update_karma(user_id, increment)
-
-    if increment == 2:
-        say(f"<@{user_id}>'s karma got a double boost! 🚀 New karma count: {karma}")
-    else:
-        say(f"<@{user_id}>'s karma took a double hit! 💔 New karma count: {karma}")
-
-@app.message(re.compile(".*<@([a-zA-Z0-9_]+)>\\s?(\\+\\+|\\-\\-).*"))
+@app.message(re.compile(".*<@([a-zA-Z0-9_]+)>\\s?(\\+\\+\\+|\\-\\-\\-|\\+\\+|\\-\\-).*"))
 def process_karma_message(say, context):
     user_id = context.matches[0]
-    action = context.matches[1]
     current_user = context.user_id
+    team_id = context.team_id
+    bot_token = os.environ.get("SLACK_BOT_TOKEN")
 
-    if user_id == current_user and action == "++":
-        say("Nice try! You can't boost your own ego. 😜")
+    increment = context.matches[1]
+
+    if not is_valid_user(user_id, bot_token):
+        return  # Do nothing if the user ID is invalid
+
+    increment_value = \
+              2 if increment == "+++"  \
+        else  1 if increment == "++" \
+        else -1 if increment == "--" \
+        else -2 if increment == "---" \
+        else  0
+    
+    if user_id == current_user and increment_value > 0:
+        say(f"Nice try! You can't boost your own ego. 😜")
         return
+    
+    karma = update_karma(user_id, team_id, increment_value)
 
-    increment = 1 if action == "++" else -1
-    karma = update_karma(user_id, increment)
-
-    if increment == 1:
+    if increment_value == 2:
+        say(f"<@{user_id}>'s karma got a double boost! 🚀 New karma count: {karma}")
+    elif increment_value == 1:
         say(f"<@{user_id}>'s karma is on the rise! 🚀 New karma count: {karma}")
-    else:
+    elif increment_value == -1:
         say(f"<@{user_id}>'s karma took a hit! 💔 New karma count: {karma}")
+    elif increment_value == -2:
+        say(f"<@{user_id}>'s karma took a double hit! 💔 New karma count: {karma}")
 
-@app.message(re.compile("<@([a-zA-Z0-9_]+)> karma"))
+@app.message(re.compile("<@([a-zA-Z0-9_]+)>\\s?karma"))
 def get_user_karma(say, context):
     user_id = context.matches[0]
-    karma = get_karma(user_id)
+    team_id = context.team_id
+    bot_token = os.environ.get("SLACK_BOT_TOKEN")
+
+    if not is_valid_user(user_id, bot_token):
+        return  # Do nothing if the user ID is invalid
+
+    karma = get_karma(user_id, team_id)
     say(f"<@{user_id}>'s current karma: {karma}")
 
 @app.message(".*")
-def ping(message, say):
+def default():
     return
 
 def create_table():
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, user_id VARCHAR(255) NOT NULL UNIQUE, karma INT NOT NULL )")
+    cursor.execute("CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, user_id VARCHAR(255) NOT NULL, team_id VARCHAR(255) NOT NULL, karma INT NOT NULL )")
     conn.commit()
 
     cursor.close()
@@ -108,4 +127,3 @@ if __name__ == "__main__":
     create_table()
     print("Starting...")
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-    
