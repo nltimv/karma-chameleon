1b0666e15dbc8f06c4494cd353cd60f213875367
nltimv <git@nltimv.com>
2024-03-30 21:50:56 +0100

    Fix pipeline

---
+++ slack/cmd/cmd_test.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "testing"
+
+func TestPass(t *testing.T) {
+	expected := 5
+	actual := 2 + 3
+
+	if expected != actual {
+		t.Errorf("got %q, wanted %q", actual, expected)
+	}
+}
